8f7ce9ddc2954ef79ff71ec47e81d5230e60231d
Jorge Torreblanca <96147522+torreblank@users.noreply.github.com>
2023-09-14 12:40:56 -0600

    total cambio de objetivo

--- main.ts
+++ main.ts
@@ -1,25 +1,35 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import data from "./data.json" assert { type: "json" };
+import { base32Encode } from 'npm:@ctrl/ts-base32';
+import { Buffer } from 'npm:buffer';
+import * as OTPAuth from "https://deno.land/x/otpauth@v9.1.4/dist/otpauth.esm.js"
+
+function validaToken(user:string, token_test:string) {
+  const issuer = 'Plan_Salud';
+  var   llave    = issuer+user;
+  llave = base32Encode(Buffer(llave));
+  let totp = new OTPAuth.TOTP({
+    issuer: issuer, label: user, algorithm: "SHA1",
+    digits: 6, period: 30, secret: llave
+  });
+  // Generate a token as string
+  let token = totp.generate();
+  return (token_test == token);
+}
 
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Welcome to dinosaur API!";
+    context.response.body = "Validar token TOTP: /usuario/token";
   })
-  .get("/api", (context) => {
-    context.response.body = data;
+  .get("/:user", (context) => {
+    if (context?.params?.user) {
+      context.response.body = "sólo 1 parámetro recibido:"+context.params.user+". Debe ser /user/token";
+    }
   })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find((item) =>
-        item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
+  .get("/:user/:token", (context) => {
+    if (context?.params?.token) {
+      context.response.body = validaToken(context.params.user, context.params.token);
     }
   });
 
