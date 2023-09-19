97f56bd35759096a4b7d6c71b4c9ab4b11c692c5
Jorge Torreblanca <96147522+torreblank@users.noreply.github.com>
2023-09-19 16:26:00 -0600

    Update main.ts
    se usan las funciones window.crypto en lugar de la library que se importaba

--- main.ts
+++ main.ts
@@ -2,29 +2,39 @@ import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors }      from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import * as OTPAuth     from "https://deno.land/x/otpauth@v9.1.4/dist/otpauth.esm.js"
 import * as base32      from "https://deno.land/std/encoding/base32.ts";
-
-import { Aes }          from "https://deno.land/x/crypto/aes.ts";
-import { Cbc, Padding } from "https://deno.land/x/crypto/block-modes.ts";
 import * as base64      from "https://deno.land/std/encoding/base64.ts";
 
 const ISSUER    = Deno.env.get("ISSUER")
 const LLAVE     = Deno.env.get("LLAVE")
 const LLAVECRYP = Deno.env.get("LLAVECRYP")
+const ALGORITMO = "AES-GCM";
 
-function encripta(texto:string, arr_size=16) {
-  const LLAVE     = new TextEncoder().encode(LLAVECRYP).subarray(0,arr_size);
-  const iv        = new Uint8Array(arr_size);
-  let data        = new TextEncoder().encode(texto);
-  const cipher    = new Cbc(Aes, LLAVE, iv, Padding.PKCS7);
-  const encrypted = cipher.encrypt(data);
-  return base64.encode(encrypted) ;
+async function encripta(data:string) {
+    const iv = new Uint8Array(
+               new TextEncoder().encode(LLAVECRYP).subarray(0,16));
+    const key =new Uint8Array( 
+               new TextEncoder().encode(LLAVECRYP).subarray(0,16));
+    const key_encoded = await crypto.subtle.importKey(
+      "raw", key.buffer, ALGORITMO, true, ["encrypt", "decrypt"],
+    );
+    const encrypted_data = await window.crypto.subtle.encrypt(
+      {name:ALGORITMO, iv: iv,}, key_encoded, new TextEncoder().encode(data),
+    );
+    return base64.encode(encrypted_data);
 }
-function decripta(texto: string, arr_size=16) {
-  const LLAVE     = new TextEncoder().encode(LLAVECRYP).subarray(0,arr_size);
-  const iv        = new Uint8Array(arr_size);
-  const decipher  = new Cbc(Aes, LLAVE, iv, Padding.PKCS7);
-  const encrypted = decipher.decrypt(base64.decode(texto));
-  return new TextDecoder().decode( encrypted ) ;
+async function decripta(data:any) {
+    data = base64.decode(data);
+    const iv = new Uint8Array(
+               new TextEncoder().encode(LLAVECRYP).subarray(0,16));
+    const key =new Uint8Array( 
+               new TextEncoder().encode(LLAVECRYP).subarray(0,16));
+    const key_encoded = await crypto.subtle.importKey(
+        "raw", key.buffer, ALGORITMO, true, ["encrypt", "decrypt"],
+    );
+    const decrypted = await window.crypto.subtle.decrypt(
+      {name:ALGORITMO, iv:iv,}, key_encoded, data,
+    );
+    return new TextDecoder().decode(decrypted);
 }
 
 function creaTotp(user:string) {
@@ -46,27 +56,27 @@ function tokenNow(user:string) {
 
 const router = new Router();
 router
-  .get("/", (context) => {
-    context.response.body = "[ API tutorial de JTK ]";
+  .get("/", (ctx:any) => {
+    ctx.response.body = "[ API tutorial de JTK ]";
   })
-  .post("/encripta", (ctx) => {
+  .post("/encripta", (ctx:any) => {
    if (ctx.request.headers.has('aencriptar')) {
-     ctx.response.body = encripta( ctx.request.headers.get('aencriptar') );
-   } else {ctx.response.body = 'ERROR: Sin dato a encriptar';}
+       ctx.response.body = (async() => {return( await encripta(ctx.request.headers.get('aencriptar')) )});
+   } else {ctx.response.body = 'Sin dato a encriptar';}
   })
-  .post("/decripta", (ctx) => {
+  .post("/decripta", (ctx:any) => {
    if (ctx.request.headers.has('adecriptar')) {
-     ctx.response.body = decripta( ctx.request.headers.get('adecriptar') );
-   } else {ctx.response.body = 'ERROR: Sin dato a decriptar';}
+       ctx.response.body = (async() => {return( await decripta(ctx.request.headers.get('adecriptar')) )});
+   } else {ctx.response.body = 'Sin dato a decriptar';}
   })
-  .get("/tokenahora/:user", (context) => {
-    if (context?.params?.user) {
-        context.response.body = tokenNow(context.params.user);
+  .get("/tokenahora/:user", (ctx:any) => {
+    if (ctx?.params?.user) {
+        ctx.response.body = tokenNow(ctx.params.user);
     } else {ctx.response.body = 'ERROR: Falta parámetro';}
   })  
-  .get("/tokenvalida/:user/:token", (context) => {
-    if (context?.params?.token) {
-      context.response.body = validaToken(context.params.user, context.params.token);
+  .get("/tokenvalida/:user/:token", (ctx:any) => {
+    if (ctx?.params?.token) {
+      ctx.response.body = validaToken(ctx.params.user, ctx.params.token);
     } else {ctx.response.body = 'ERROR: Sin parámetros completos';}
   });
 
